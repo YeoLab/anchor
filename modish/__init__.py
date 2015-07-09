ee5ad56d4c08c0ee1c68f1b2892a069650671638
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-07-08 17:11:04 -0700

    fix lint

--- modish/__init__.py
+++ modish/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
+from model import ModalityModel
+from estimator import ModalityEstimator
 
 __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
 __version__ = '0.1.0'
 
-from model import ModalityModel
-from estimator import ModalityEstimator
 
 __all__ = ['ModalityModel', 'ModalityEstimator']
