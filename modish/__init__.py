82b1a0a48a1690f7a19dbe62085a6fca9193ba08
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-07-08 17:20:20 -0700

    python3 relative imports

--- modish/__init__.py
+++ modish/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from model import ModalityModel
-from estimator import ModalityEstimator
+from .model import ModalityModel
+from .estimator import ModalityEstimator
 
 __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
