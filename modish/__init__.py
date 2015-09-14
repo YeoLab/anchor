b45e34e63454fef3ad3a220cea914818c447810c
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-09-13 20:50:59 -0700

    add MODALITY_{COLORS,ORDER} to __init__ for accessibility

--- modish/__init__.py
+++ modish/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from .model import ModalityModel
 from .estimator import ModalityEstimator
+from .visualize import MODALITY_COLORS, MODALITY_ORDER, ModalitiesViz
 
 __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
