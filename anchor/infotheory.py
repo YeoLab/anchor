5df429fd18fc80bae669656d691f38d941236360
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-05-13 10:58:53 -0700

    Specify range=(0,1) otherwise np.histogram breaks when there's NAs

--- anchor/infotheory.py
+++ anchor/infotheory.py
@@ -63,9 +63,10 @@ def binify(data, bins):
     if bins is None:
         raise ValueError('Must specify "bins"')
     if isinstance(data, pd.DataFrame):
-        binned = data.apply(lambda x: pd.Series(np.histogram(x, bins=bins)[0]))
+        binned = data.apply(lambda x: pd.Series(np.histogram(x, bins=bins,
+                                                             range=(0, 1))[0]))
     elif isinstance(data, pd.Series):
-        binned = pd.Series(np.histogram(data, bins=bins)[0])
+        binned = pd.Series(np.histogram(data, bins=bins, range=(0, 1))[0])
     else:
         raise ValueError('`data` must be either a 1d vector or 2d matrix')
     binned.index = bin_range_strings(bins)
